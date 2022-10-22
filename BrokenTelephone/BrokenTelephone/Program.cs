342ec9fa37d53c5737eb17d38fbabf08272517ab
SH-NicholasPage <npage@southhills.edu>
2022-10-21 22:05:10 -0400

    Initial commit

---
+++ BrokenTelephone/BrokenTelephone/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace BrokenTelephone
+{
+    public sealed class Program
+    {
+        private const byte MAX_SCORE = 20;
+
+        public static void Main()
+        {
+            if(File.Exists("inputs.txt") == false)
+            {
+                Console.Error.WriteLine("ERROR: Input file could not be found! Tell the instructor about this error message ASAP.");
+            }
+
+            String[] lines = File.ReadAllLines("inputs.txt");
+
+            float score = MAX_SCORE;
+            float detractor = MAX_SCORE / lines.Length;
+
+            for(int i = 0; i < lines.Length; i += 2)
+            {
+                int[] numbers = lines[i].Split(" ").Select(x => Convert.ToInt32(x)).ToArray();
+                int answer = Convert.ToInt32(lines[i + 1]);
+
+                int studentResult = Source.Solve(numbers);
+
+                if(studentResult != answer)
+                {
+                    Console.WriteLine($"Answer for input {i/2} ({studentResult}) was incorrect.");
+                    score -= detractor;
+                }
+            }
+
+            Console.WriteLine($"***Final score: {Math.Round(score, 2)}/{MAX_SCORE}");
+        }
+    }
+}
---
+++ BrokenTelephone/BrokenTelephone/Source.cs
@@ -0,0 +1,26 @@
+﻿/*
+* Member 1:
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+* Member 2:
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+* Member 3: (delete or leave empty if not applicable)
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BrokenTelephone
+{
+    public sealed class Source
+    {
+        public static int Solve(int[] playerMessages)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
